67f0290405bfe5e805d8658f860af5073fc452d5
Fernandasacosta <107959631+Fernandasacosta@users.noreply.github.com>
2024-03-05 12:54:11 -0300

    aplicando audio e estilos

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ const banner = document.querySelector('.app__image')
 
 const titulo = document.querySelector('.app__title')
 
+const botoes = document.querySelectorAll('.app__card-button')
+
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+musica.loop = true 
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
+})
+
 
 focoBtn.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBtn.classList.add('active')
     // html.setAttribute('data-contexto', 'foco')
     // banner.setAttribute('src', '/imagens/foco.png')
 })
@@ -18,15 +33,20 @@ curtoBtn.addEventListener('click', () => {
     alterarContexto('descanso-curto')
     // html.setAttribute('data-contexto', 'descanso-curto')
     // banner.setAttribute('src', '/imagens/descanso-curto.png')
+    curtoBtn.classList.add('active')
 })
 
 longoBtn.addEventListener('click', () => {
     alterarContexto('descanso-longo')
     // html.setAttribute('data-contexto', 'descanso-longo')
     // banner.setAttribute('src', '/imagens/descanso-longo.png')
+    longoBtn.classList.add('active')
 })
 
 function alterarContexto (contexto){
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch(contexto){
@@ -34,7 +54,6 @@ function alterarContexto (contexto){
             titulo.innerHTML =
             ` Otimize sua produtividade,<br>
             <strong class="app__title-strong">mergulhe no que importa.</strong>`
-
 
             break;
         case "descanso-curto":
