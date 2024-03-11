34b30c4649925b766cc6beaf35ccfee5394c58cf
Fernandasacosta <107959631+Fernandasacosta@users.noreply.github.com>
2024-03-11 14:44:10 -0300

    consumindo api json

---
+++ script-crud.js
@@ -0,0 +1,23 @@
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = [
+
+]
+
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
+
+
+//quando clicar em salvar no formulário ele não recarregara a página
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao : textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+})
